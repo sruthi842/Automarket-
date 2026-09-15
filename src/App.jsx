e980ff92be88929da7907cc5b871e31875736d9b
Keerthi Sruthi <keerthisruthi92@gmail.com>
2026-09-15 09:19:17 +0530

    Fix login import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useMemo, useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 import VehicleDetails from "./pages/VehicleDetails";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import SellVehicle from "./pages/sellvehicle";
 import ContactSeller from "./pages/contactseller";
 
